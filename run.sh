84fb5d9f6ff63c9d30dbae4a2a84c5cad0db9bae
siaga <sliuweimin@gmail.com>
2017-11-29 21:27:02 +0800

    fix

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
 chown root:www-data /etc/apache2/webdav.password
 chmod 640 /etc/apache2/webdav.password
 
+rm -f /var/run/apache2.pid
+
 apache2 -D FOREGROUND
